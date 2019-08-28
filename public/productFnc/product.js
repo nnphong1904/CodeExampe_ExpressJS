95a854ddfe509cb713ebde8630c91609978c99d7
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-08-28 21:17:32 +0700

    Completed pagination

--- public/productFnc/product.js
+++ public/productFnc/product.js
@@ -1,6 +1,9 @@
 var nextFunction=function(){
+        var previousBtn=document.getElementById('previous-btn');
+                previousBtn.classList.remove('disabled');
    var paginationBtn=document.querySelectorAll('#pagination-btn');
         paginationBtn.forEach(function(item){
+           
            item.textContent=(parseInt(item.textContent)+3).toString();
            item.setAttribute('href','/product?page='+item.textContent);
             if (parseInt(item.textContent)===13)
@@ -8,8 +11,25 @@ var nextFunction=function(){
                 var nextBtn=document.getElementById('next-btn');
                 nextBtn.classList.add('disabled');
             }
-           if ((parseInt(item.textContent))>13){
+            if ((parseInt(item.textContent))>13){
                  item.setAttribute('style','display:none');
             }
         });     
+}
+
+var previousFunction=function(){
+        var nextBtn=document.getElementById('next-btn');
+          nextBtn.classList.remove('disabled');
+        var paginationBtn=document.querySelectorAll('#pagination-btn');
+        paginationBtn.forEach(function(item){
+                item.removeAttribute('style');
+                item.textContent=(parseInt(item.textContent)-3).toString();
+                item.setAttribute('href','/product?page='+item.textContent);
+                
+                if (parseInt(item.textContent)===1)
+                {
+                        var previousBtn=document.getElementById('previous-btn');
+                        previousBtn.classList.add('disabled');
+                }
+        });
 }
